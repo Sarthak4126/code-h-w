c973773ffe36de3e1ae37598a7e8f0579add1cb4
Sarthak Manhas <146426131+Sarthak4126@users.noreply.github.com>
2024-01-06 13:54:30 +0530

    Add files via upload

---
+++ primeno.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+//main function
+int main()
+{
+//variable declaration and assignment of value
+int num,i=1,c=0;
+//inputr statement
+printf("Enter a number");
+scanf("%d",&num);
+//while loop statement
+while(i<=num)
+{
+//if else condition
+if(num%i==0)
+c++;
+i++;
+}
+if(c==2)
+printf("\n%d is a prime number",num);
+else
+printf("\n%d is not a prime number",num);
+//return statement
+return 0;
+}
---
+++ sumofsqurerec.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+//main function heading
+void main()
+{
+//variable declaration
+int num,sum;
+int sumofsquares (int);
+//input statement from user
+printf("Enter the number ");
+scanf("%d",&num);
+//assignment of value
+sum=sumofsquares(num);
+//output statement
+printf("sum of squares of given number(factorial)=%d",sum);
+}
+//taking function for calculating sum of squares of factorial a number
+int sumofsquares(int n)
+{
+//if else statement
+if(n==1)
+//return statement(if part)
+return 1;
+else
+//return statement(else part)
+return (n*n)+sumofsquares(n-1);
+}
